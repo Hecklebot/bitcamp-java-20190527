f208ac82daef2321e64b53905d2731b1a3748ae8
Hecklebot <51806574+Hecklebot@users.noreply.github.com>
2019-07-04 09:11:39 +0900

    Update App.java

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
@@ -11,6 +11,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        System.out.println("this code write by labtop");
+        System.out.println("this code written by labtop");
     }
 }
