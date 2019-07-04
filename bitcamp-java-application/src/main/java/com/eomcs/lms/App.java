8e5e79da511313dc4775d2adbe535814a48538d9
Sohansaem <iuehakdrk@gmail.com>
2019-07-04 09:03:47 +0900

    test

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
@@ -11,5 +11,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println("this code write by labtop");
     }
 }
