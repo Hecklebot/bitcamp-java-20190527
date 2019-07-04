71748856f747f208fe164c012aceb26d28b16ea5
sohansaem <iuehakdrk@gmail.com>
2019-07-04 17:43:26 +0900

    ch04 complete

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App2.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App2.java
@@ -1,15 +1,38 @@
 package com.eomcs.lms;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class App2 {
 
   public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("이름: 홍길동");
-    System.out.println("이메일: hong@test.com");
-    System.out.println("암호: 1111");
-    System.out.println("사진: hong.png");
-    System.out.println("전화: 1111-2222");
+    
+    InputStream keyboard = System.in;
+    Scanner scan = new Scanner(keyboard);
+    
+    System.out.print("번호? ");
+    String number = scan.nextLine();
+    System.out.print("이름? ");
+    String name = scan.nextLine();
+    System.out.print("이메일? ");
+    String eMail = scan.nextLine();
+    System.out.print("암호? ");
+    String password = scan.nextLine();
+    System.out.print("사진? ");
+    String picture = scan.nextLine();
+    System.out.print("전화? ");
+    String phoneNumber = scan.nextLine();
+    
+    System.out.println();
+    
+    System.out.println("번호: " + number);
+    System.out.println("이름: " + name);
+    System.out.println("이메일: " + eMail);
+    System.out.println("암호: " + password);
+    System.out.println("사진: " + picture);
+    System.out.println("전화: " + phoneNumber);
     System.out.println("가입일: 2019-01-01");
+    
 
   }
 
--- bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
@@ -1,10 +1,26 @@
 package com.eomcs.lms;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class App3 {
   public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("내용: 게시글입니다.");
+    InputStream keyboard = System.in;
+    Scanner scan = new Scanner(keyboard);
+    
+    System.out.print("번호? ");
+    String number = scan.nextLine();
+    
+    System.out.print("내용? ");
+    String input = scan.nextLine();
+        
+    System.out.println();
+    
+    System.out.println("번호: " + number);
+    System.out.println("내용: " + input);
     System.out.println("작성일: 2019-01-01");
     System.out.println("조회수: 0");
+
+    
   }
 }
