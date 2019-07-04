61fa09f47cc725ca89839ccbe3cc25b6a4775fc5
Hecklebot <51806574+Hecklebot@users.noreply.github.com>
2019-07-04 12:00:29 +0900

    Update AppTest.java
    add test code

--- bitcamp-java-application/src/test/java/com/eomcs/lms/AppTest.java
+++ bitcamp-java-application/src/test/java/com/eomcs/lms/AppTest.java
@@ -12,5 +12,6 @@ public class AppTest {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.println("this code written by labtop");
+        System.out.println("this is test code")
     }
 }
