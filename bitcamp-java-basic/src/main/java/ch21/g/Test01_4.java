e94759638e1c32e10e8de3108ee2301c81132f62
sohansaem <iuehakdrk@gmail.com>
2019-07-24 17:54:29 +0900

    bitcamp-java-basic ch21 update complete

--- bitcamp-java-basic/src/main/java/ch21/g/Test01_4.java
+++ bitcamp-java-basic/src/main/java/ch21/g/Test01_4.java
@@ -9,9 +9,6 @@ public class Test01_4 {
   static FileReader in;
   public static void main(String[] args) {
     
-    //여기서 예외가 발생하면 main()의 호출자인 JNM에게 전달될 것이다.
-    //이건 되긴 하는데 main()의 호출자는 JVM이기 때문에 어차피 예외가 발생한다.
-    
     try {
       m1();
     } catch(FileNotFoundException e) {
