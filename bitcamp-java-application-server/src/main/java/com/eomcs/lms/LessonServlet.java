935cb3bde3a032ce49b8c9b32a518ab0126ab6fc
sohansaem <iuehakdrk@gmail.com>
2019-08-02 14:29:59 +0900

    bitcamp-java-application v32_8 complete

--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/LessonServlet.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/LessonServlet.java
@@ -4,16 +4,16 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.domain.Lesson;
 
 // 게시물 요청을 처리하는 담당자
-public class BoardServlet implements Servlet {
+public class LessonServlet implements Servlet {
   
-  ArrayList<Board> boardList = new ArrayList<>();
+  ArrayList<Lesson> lessonList = new ArrayList<>();
   ObjectInputStream in;
   ObjectOutputStream out;
   
-  public BoardServlet(ObjectInputStream in, ObjectOutputStream out) {
+  public LessonServlet(ObjectInputStream in, ObjectOutputStream out) {
     this.in = in;
     this.out = out;
   }
@@ -21,20 +21,20 @@ public class BoardServlet implements Servlet {
   @Override
   public void service(String command) throws Exception {
     switch (command) {
-      case "/board/add":
-        addBoard();
+      case "/lesson/add":
+        addLesson();
         break;
-      case "/board/list":
-        listBoard();
+      case "/lesson/list":
+        listLesson();
         break;
-      case "/board/delete":
-        deleteBoard();
+      case "/lesson/delete":
+        deleteLesson();
         break;
-      case "/board/detail":
-        detailBoard();
+      case "/lesson/detail":
+        detailLesson();
         break;
-      case "/board/update":
-        updateBoard();
+      case "/lesson/update":
+        updateLesson();
         break;
       default:
         out.writeUTF("fail");
@@ -42,23 +42,23 @@ public class BoardServlet implements Servlet {
     }
   }
 
-  private void addBoard() throws ClassNotFoundException, IOException {
+  private void addLesson() throws ClassNotFoundException, IOException {
     // 클라리언트가 보낸 객체를 읽는다.
-    Board board = (Board) in.readObject();
-    boardList.add(board);
+    Lesson lesson = (Lesson) in.readObject();
+    lessonList.add(lesson);
     out.writeUTF("ok");
   }
 
-  private void listBoard() throws IOException {
+  private void listLesson() throws IOException {
     out.writeUTF("ok");
     out.reset(); // 기존에 serialize 했던 객체의 상태를 무시하고 다시 serialize 한다.
-    out.writeObject(boardList);
+    out.writeObject(lessonList);
   }
 
-  private void detailBoard() throws IOException {
+  private void detailLesson() throws IOException {
     int no = in.readInt();
 
-    int index = indexOfBoard(no);
+    int index = indexOfLesson(no);
     // if문에서는 부정적인 조건을 찾는게 코딩이 수월하다 왜?
     // 코드의 흐름을 들여쓰기로 구분하는게 좋다. 다른 들여쓰기 -> 다른 흐름
 
@@ -67,7 +67,7 @@ public class BoardServlet implements Servlet {
       return;
     }
     out.writeUTF("ok");
-    out.writeObject(boardList.get(index));
+    out.writeObject(lessonList.get(index));
 
 
     // for (Member m : memberList) {
@@ -84,17 +84,17 @@ public class BoardServlet implements Servlet {
 
   }
 
-  private void updateBoard() throws IOException, ClassNotFoundException {
+  private void updateLesson() throws IOException, ClassNotFoundException {
     // 인덱스를 알고 있을 때, 사용할 수 있는 방법
-    Board board = (Board) in.readObject();
-    int index = indexOfBoard(board.getNo());
+    Lesson lesson = (Lesson) in.readObject();
+    int index = indexOfLesson(lesson.getNo());
 
     if (index == -1) {
       fail("해당 번호의 게시물이 없습니다.");
       return;
     }
 
-    boardList.set(index, board);
+    lessonList.set(index, lesson);
     out.writeUTF("ok");
 
     // for (int i = 0; i < memberList.size(); i++) {
@@ -109,16 +109,16 @@ public class BoardServlet implements Servlet {
 
   }
 
-  private void deleteBoard() throws IOException {
+  private void deleteLesson() throws IOException {
     int no = in.readInt();
 
-    int index = indexOfBoard(no);
+    int index = indexOfLesson(no);
     if (index == -1) {
       fail("해당 번호의 게시물이 없습니다.");
       return;
     }
 
-    boardList.remove(index);
+    lessonList.remove(index);
     out.writeUTF("ok");
 
     // for (Member m : memberList) {
@@ -134,9 +134,9 @@ public class BoardServlet implements Servlet {
 
   }
 
-  private int indexOfBoard(int no) {
+  private int indexOfLesson(int no) {
     int i = 0;
-    for (Board b : boardList) {
+    for (Lesson b : lessonList) {
       if (b.getNo() == no) {
         return i;
       }
