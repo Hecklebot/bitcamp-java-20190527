336b0f756e6b6efd8327d2945e58fc21aeb42e1e
sohansaem <iuehakdrk@gmail.com>
2019-08-27 10:54:53 +0900

    bitcamp-java-application-server v43_1 complete

---
+++ bitcamp-java-application4-server/v43_1/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
@@ -0,0 +1,75 @@
+package com.eomcs.lms.dao.mariadb;
+
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.lms.domain.Board;
+
+public class BoardDaoImpl implements BoardDao {
+
+  SqlSessionFactory sqlSessionFactory;
+
+  public BoardDaoImpl(SqlSessionFactory sqlSessionFactory) {
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  @Override
+  public int insert(Board board) throws Exception {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      int count = sqlSession.insert("BoardDao.insert", board);
+      sqlSession.commit();
+      return count;
+
+    } catch(Exception e) {
+      sqlSession.rollback();
+      throw e;
+
+    } finally {
+      sqlSession.close();
+    }
+  }
+
+  @Override
+  public List<Board> findAll() throws Exception {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      return sqlSession.selectList("BoardDao.findAll");
+    }
+  }
+
+  @Override
+  public Board findBy(int no) throws Exception {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      Board board = sqlSession.selectOne("BoardDao.findBy", no);
+
+      if (board != null) {
+        sqlSession.update("BoardDao.increaseViewCount", no);
+      }
+      sqlSession.commit();
+      return board;
+    } catch(Exception e) {
+      sqlSession.rollback();
+      throw e;
+    } finally {
+      sqlSession.close();
+    }
+  }
+
+  @Override
+  public int update(Board board) throws Exception {
+    // openSession()을 호출할 때, 다음과 같이 boolean값을 넣으면 autoCommit 여부를 설정할 수 있다.
+    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+      return sqlSession.update("BoardDao.update", board);
+    }
+  }
+
+  @Override
+  public int delete(int no) throws Exception {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)){
+      return sqlSession.delete("BoardDao.delete", no);
+    }
+  }
+
+}
---
+++ bitcamp-java-application4-server/v43_1/src/main/java/com/eomcs/lms/handler/LessonUpdateCommand.java
@@ -0,0 +1,53 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.sql.Date;
+import com.eomcs.lms.dao.LessonDao;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.util.Input;
+
+public class LessonUpdateCommand implements Command {
+
+  private LessonDao lessonDao;
+
+  public LessonUpdateCommand(LessonDao lessonDao) {
+    this.lessonDao = lessonDao;
+  }
+
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+
+    try {
+      int no = Input.getIntValue(in, out, "번호? ");
+      Lesson lesson = lessonDao.findBy(no);
+      if (lesson == null) {
+        out.println("해당 번호의 데이터가 없습니다!");
+        return;
+      }
+
+      lesson.setTitle(Input.getStringValue(in, out, "수업명(" + lesson.getTitle() + ")? "));
+
+      lesson.setContents(Input.getStringValue(in, out, "수업내용"));
+
+      lesson.setStartDate(
+          Date.valueOf(Input.getStringValue(in, out, "시작일(" + lesson.getStartDate() + ")? ")));
+
+      lesson.setEndDate(
+          Date.valueOf(Input.getStringValue(in, out, "종료일(" + lesson.getEndDate() + ")? ")));
+
+      lesson.setContents(Input.getStringValue(in, out, "총수업시간(" + lesson.getTotalHours() + ")? "));
+
+      lesson.setContents(Input.getStringValue(in, out, "일수업시간(" + lesson.getDayHours() + ")? "));
+
+      lessonDao.update(lesson);
+      out.println("데이터를 변경하였습니다.");
+    } catch (Exception e) {
+      out.println("데이터 변경에 실패했습니다!");
+      System.out.println(e.getMessage());
+    }
+  }
+
+}
+
+
